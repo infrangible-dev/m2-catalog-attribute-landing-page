aff8c8224dc1152486309245077b3e4803a9efb5
Andreas Knollmann <a.knollmann@seak.dev>
2024-11-02 03:08:06 +0100

    added separate attribute list to avoid plugins

--- src/Model/Layer/FilterList.php
+++ src/Model/Layer/FilterList.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Infrangible\CatalogAttributeLandingPage\Model\Layer;
 
+use Infrangible\CatalogAttributeLandingPage\Model\Layer\Category\FilterableAttributeList;
 use Infrangible\CatalogAttributeLandingPage\Model\Layer\Filter\Attribute;
 use Infrangible\CatalogAttributeLandingPage\Model\Layer\Filter\AttributeSet;
 use Magento\Catalog\Model\Config\LayerCategoryConfig;
 use Magento\Catalog\Model\Layer;
-use Magento\Catalog\Model\Layer\Category\FilterableAttributeList;
 use Magento\Catalog\Model\Layer\Filter\AbstractFilter;
 use Magento\CatalogSearch\Model\Layer\Filter\Category;
 use Magento\CatalogSearch\Model\Layer\Filter\Decimal;
@@ -20,20 +20,25 @@ use Magento\Framework\ObjectManagerInterface;
  * @copyright   Copyright (c) 2014-2024 Softwareentwicklung Andreas Knollmann
  * @license     http://www.opensource.org/licenses/mit-license.php MIT
  */
-class FilterList
-    extends Layer\FilterList
+class FilterList extends Layer\FilterList
 {
     public function __construct(
         ObjectManagerInterface $objectManager,
         FilterableAttributeList $filterableAttributes,
-        LayerCategoryConfig $layerCategoryConfig)
-    {
-        parent::__construct($objectManager, $filterableAttributes, $layerCategoryConfig, [
-            'attribute' => Attribute::class,
-            'price' => Price::class,
-            'decimal' => Decimal::class,
-            'category' => Category::class,
-            'attribute_set' => AttributeSet::class]);
+        LayerCategoryConfig $layerCategoryConfig
+    ) {
+        parent::__construct(
+            $objectManager,
+            $filterableAttributes,
+            $layerCategoryConfig,
+            [
+                'attribute' => Attribute::class,
+                'price' => Price::class,
+                'decimal' => Decimal::class,
+                'category' => Category::class,
+                'attribute_set' => AttributeSet::class
+            ]
+        );
     }
 
     /**
@@ -41,10 +46,13 @@ class FilterList
      */
     public function getFilters(Layer $layer): array
     {
-        if (!count($this->filters)) {
+        if (! count($this->filters)) {
             parent::getFilters($layer);
 
-            $this->filters[] = $this->objectManager->create($this->filterTypes['attribute_set'], ['layer' => $layer]);
+            $this->filters[] = $this->objectManager->create(
+                $this->filterTypes[ 'attribute_set' ],
+                ['layer' => $layer]
+            );
         }
 
         return $this->filters;
